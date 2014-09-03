d8fc82628bc43dda7b44d2521e37bae8e73f6aa6
David Höppner <0xffea@gmail.com>
2014-09-03 20:12:28 +0200

    PlaceNames in serializer

--- src/main/java/eu/skqs/bertie/cas/TeiCasSerializer.java
+++ src/main/java/eu/skqs/bertie/cas/TeiCasSerializer.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package eu.skqs.bertie.cas;
 
 import java.io.OutputStream;
@@ -69,6 +88,18 @@ public class TeiCasSerializer {
 				startPosition = annotation.getEnd();
 			} else if (annotationName.equals("eu.skqs.type.PersName")) {
 				tagName = "persName";
+				endPosition = annotation.getBegin();
+
+				result += documentText.substring(startPosition, endPosition);
+				result += "\n";
+				result += "<" + tagName + ">";
+				result += annotation.getCoveredText();
+				result += "</" + tagName + ">";
+				result += "\n";
+
+				startPosition = annotation.getEnd();
+			} else if (annotationName.equals("eu.skqs.type.PlaceName")) {
+				tagName = "placeName";
 				endPosition = annotation.getBegin();
 
 				result += documentText.substring(startPosition, endPosition);
